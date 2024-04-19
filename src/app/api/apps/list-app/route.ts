5e64fc3de6f7909259897a0f6fe45ecbfc51cf85
bipproduction <bip.production.js@gmail.com>
2024-04-19 17:00:02 +0800

    auto push

--- src/app/api/apps/list-app/route.ts
+++ src/app/api/apps/list-app/route.ts
@@ -1,20 +1,17 @@
-// import { spawn } from 'child_process'
+import { spawn } from 'child_process'
 
 export async function GET() {
-    // const child = spawn('/bin/sh', ["-c", "pm2 jlist"])
-    // let log: string[] = []
-    // child.stdout.on('data', (data) => {
-    //     log = JSON.parse(`${data}`)
-    // })
+    const child = spawn('/bin/sh', ["-c", "pm2 jlist"])
+    let log: string[] = []
+    child.stdout.on('data', (data) => {
+        log = JSON.parse(data.toString())
+    })
 
-    // await new Promise(resolve => setTimeout(resolve, 1000))
-
-    // return Response.json({
-    //     success: true,
-    //     message: log,
-    // })
+    await new Promise(resolve => setTimeout(resolve, 1000))
 
     return Response.json({
-        
+        success: true,
+        message: log,
     })
+
 }
