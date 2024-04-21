e6b7dc47f9287d9138edc48c3bfd64141e6e77ec
bipproduction <bip.production.js@gmail.com>
2024-04-21 14:03:31 +0800

    auto push

--- src/app/api/dev/build/route.ts
+++ src/app/api/dev/build/route.ts
@@ -2,6 +2,7 @@
 import appASetting from '@/util/app_setting';
 import { spawn } from 'child_process'
 
+
 export async function GET() {
 
     if (appASetting.isLocal) return new Response("Not Available on Local", { status: 500 })
