73805a832decf241c9724aeed28fd52b8d307fcb
bipproduction <bip.production.js@gmail.com>
2024-04-21 23:06:06 +0800

    tambahan

--- src/app/api/dev/build/route.ts
+++ src/app/api/dev/build/route.ts
@@ -2,7 +2,7 @@ import build from '@/bin/build';
 import appASetting from '@/util/app_setting';
 
 export async function GET(req: Request, { searchParams }: { searchParams: { cmd: string } }) {
-    const cmd = (searchParams && searchParams.cmd) ?? "ls"
+    const cmd = "git pull origin main && yarn build && pm2 restart wibu-app_3025"
     if (appASetting.isLocal) return new Response("Not Available on Local", { status: 500 })
 
     const stream = build({ cmd: cmd })
