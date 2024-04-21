8a4ca977f2ba6b96a791d0cca641c70486b26964
bipproduction <bip.production.js@gmail.com>
2024-04-21 19:50:27 +0800

    tambahan

--- src/app/api/dev/build/route.ts
+++ src/app/api/dev/build/route.ts
@@ -4,15 +4,19 @@ import appASetting from '@/util/app_setting';
 export async function GET() {
     if (appASetting.isLocal) return new Response("Not Available on Local", { status: 500 })
 
-    // const stream =  build()
+    try {
+        const stream = build()
 
-    // Return the response with the stream
-    return new Response("stream", {
-        headers: {
-            'Content-Type': 'text/event-stream',
-            'Cache-Control': 'no-cache',
-        },
-    });
+        // Return the response with the stream
+        return new Response("stream", {
+            headers: {
+                'Content-Type': 'text/event-stream',
+                'Cache-Control': 'no-cache',
+            },
+        });
+    } catch (error) {
+        return new Response("error", { status: 500 })
+    }
 
 }
 
