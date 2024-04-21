4d48a88812f36f94d7f86d2e885c9176642901f0
bipproduction <bip.production.js@gmail.com>
2024-04-21 19:45:10 +0800

    tambahan

--- src/app/api/dev/build/route.ts
+++ src/app/api/dev/build/route.ts
@@ -4,10 +4,10 @@ import appASetting from '@/util/app_setting';
 export async function GET() {
     if (appASetting.isLocal) return new Response("Not Available on Local", { status: 500 })
 
-   const stream = await build()
+//    const stream = await build()
 
     // Return the response with the stream
-    return new Response(stream, {
+    return new Response("stream", {
         headers: {
             'Content-Type': 'text/event-stream',
             'Cache-Control': 'no-cache',
