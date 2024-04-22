748ab6f8463d896f808b6465194104f728e2c1da
bipproduction <bip.production.js@gmail.com>
2024-04-22 10:33:50 +0800

    tambahan

--- src/app/api/dev/build/route.ts
+++ src/app/api/dev/build/route.ts
@@ -1,12 +1,31 @@
 import build from '@/bin/build';
 import appASetting from '@/util/app_setting';
+import { spawn } from 'child_process'
 
 export async function GET(req: Request) {
-    const query_cmd = new URL(req.url).searchParams.get('cmd')
-    const cmd = query_cmd || "git pull origin main && yarn build && pm2 restart wibu-app_3025"
+
     if (appASetting.isLocal) return new Response("Not Available on Local", { status: 500 })
 
-    const stream = build({ cmd: cmd})
+    const stream = new ReadableStream({
+        start(controller) {
+            const child = spawn('/bin/sh', ['-c', "git pull origin main && yarn build && pm2 restart wibu-app_3025"]);
+            // Handle stdout data from the child process
+            child.stdout.on('data', (data) => {
+                // Push data into the stream
+                controller.enqueue(data);
+            });
+
+            child.stderr.on('data', (data) => {
+                // Push data into the stream
+                controller.enqueue(data);
+            })
+            // Handle the end of the child process
+            child.on('close', () => {
+                // Close the stream
+                controller.close();
+            });
+        }
+    });
 
     // Return the response with the stream
     return new Response(stream, {
