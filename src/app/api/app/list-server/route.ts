0da88d547d94ec937c1e4c7b359677671e076788
bipproduction <bip.production.js@gmail.com>
2024-04-22 11:57:10 +0800

    update

--- src/app/api/app/list-server/route.ts
+++ src/app/api/app/list-server/route.ts
@@ -1,18 +1,20 @@
 import { spawn } from 'child_process'
+import fs from 'fs'
 export async function GET() {
-    const child = spawn('/bin/sh', ["-c", "ls /etc/nginx/sites-enabled"])
+    const dir = await fs.promises.readdir('/etc/nginx/sites-enabled')
+    // const child = spawn('/bin/sh', ["-c", "ls /etc/nginx/sites-enabled"])
 
-    let log: string[] = []
+    // let log: string[] = []
 
-    child.stdout.on('data', (data) => {
-        log = `${data}`.split('\n').filter(x => x !== '')
-    })
+    // child.stdout.on('data', (data) => {
+    //     log = `${data}`.split('\n').filter(x => x !== '')
+    // })
 
-    child.stderr.on('data', (data) => {
-        console.log(data.toString())
-    })
+    // child.stderr.on('data', (data) => {
+    //     console.log(data.toString())
+    // })
 
-    await new Promise(resolve => setTimeout(resolve, 1000))
+    // await new Promise(resolve => setTimeout(resolve, 1000))
 
-    return Response.json(log)
+    return Response.json(dir)
 }
