10bc8d091c371335b583f9e97a8c26bdf81d08be
bipproduction <bip.production.js@gmail.com>
2024-04-22 10:39:06 +0800

    tambahan

--- src/app/api/dev/build/route.ts
+++ src/app/api/dev/build/route.ts
@@ -7,22 +7,25 @@ export async function GET(req: Request) {
     if (appASetting.isLocal) return new Response("Not Available on Local", { status: 500 })
 
     if (cmd) return new Response("OK", { status: 200 })
-    
+
     const stream = new ReadableStream({
         start(controller) {
             const child = spawn('/bin/sh', ['-c', "git pull origin main && yarn build && pm2 restart wibu-app_3025"]);
             // Handle stdout data from the child process
             child.stdout.on('data', (data) => {
+                console.log(data.toString())
                 // Push data into the stream
                 controller.enqueue(data);
             });
 
             child.stderr.on('data', (data) => {
+                console.log(data.toString())
                 // Push data into the stream
                 controller.enqueue(data);
             })
             // Handle the end of the child process
             child.on('close', () => {
+                console.log("selesai")
                 // Close the stream
                 controller.close();
             });
