5a4aeaff747816f23db9a9207f2a6ac6b40e1919
bipproduction <bip.production.js@gmail.com>
2024-04-22 10:56:21 +0800

    build

--- src/app/api/dev/test/route.ts
+++ src/app/api/dev/test/route.ts
@@ -1,45 +1,33 @@
-import build from '@/bin/build';
-import appASetting from '@/util/app_setting';
 import { spawn } from 'child_process'
 
-export async function GET(req: Request) {
-    const cmd = new URL(req.url).searchParams.get('cmd')
-    if (appASetting.isLocal) return new Response("Not Available on Local", { status: 500 })
-
-    if (cmd) return new Response(cmd, { status: 200 })
+export async function GET() {
 
     const stream = new ReadableStream({
         start(controller) {
-            const child = spawn('/bin/sh', ['-c', "git pull origin main && yarn build && pm2 restart wibu-app_3025"]);
+            const child = spawn('/bin/sh', ['bin/build.sh'])
             // Handle stdout data from the child process
             child.stdout.on('data', (data) => {
-                console.log(data.toString())
                 // Push data into the stream
                 controller.enqueue(data);
             });
 
             child.stderr.on('data', (data) => {
-                console.log(data.toString())
                 // Push data into the stream
                 controller.enqueue(data);
             })
+
             // Handle the end of the child process
             child.on('close', () => {
-                console.log("selesai")
                 // Close the stream
                 controller.close();
-            });
+            })
         }
-    });
+    })
 
-    // Return the response with the stream
     return new Response(stream, {
         headers: {
             'Content-Type': 'text/event-stream',
             'Cache-Control': 'no-cache',
         },
-    });
-
+    })
 }
-
-
