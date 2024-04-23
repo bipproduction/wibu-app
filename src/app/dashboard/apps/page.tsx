ba9715e29f24bffdb363d249c0cdf2adea865e87
bipproduction <bip.production.js@gmail.com>
2024-04-23 20:05:35 +0800

    build

--- src/app/dashboard/apps/page.tsx
+++ src/app/dashboard/apps/page.tsx
@@ -1,7 +1,6 @@
 import AppsUi from "@/ui/AppsUi";
 import Project from "@/ui/Project";
 import Server from "@/ui/Server";
-import TestUi from "@/ui/TestUi";
 
 import appASetting from "@/util/app_setting";
 import { Stack, Title } from "@mantine/core";
@@ -15,13 +14,11 @@ export default async function Page({ params }: any) {
         const listProject = await fetch(`${appASetting.url}/api/app/list-project`).then(res => res.json())
 
         return <Stack>
-            {/* <TestUi /> */}
             <AppsUi listApp={listApp}  />
             <Project listProject={listProject} />
             <Server listServer={listServer} />
         </Stack>
     } catch (error) {
-        // console.log(error)
         return <>{JSON.stringify(error)}</>
     }
 }
