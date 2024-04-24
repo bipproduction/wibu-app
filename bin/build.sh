31a4fba37c1ff188c633937ad24c28174f24be51
bipproduction <bip.production.js@gmail.com>
2024-04-24 12:01:16 +0800

    build

--- bin/build.sh
+++ bin/build.sh
@@ -1,10 +1,10 @@
 # git pull origin build
 yarn install
 npx prisma db push
-yarn dev --port 3333 &
-pid=$!
-echo $pid
-sleep 10
-kill $pid
+# yarn dev --port 3333 &
+# pid=$!
+# echo $pid
+# sleep 10
+# kill $pid
 yarn build
 pm2 restart wibu-app_3025
